2d9ed3b6d258c0b40d87c2b866095911863caaa4
andy-blandy <shsandrewdaniels@gmail.com>
2025-09-16 12:28:56 -0400

    completed more javascript exercises about objects

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((prev, num) => prev + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((prev, num) => prev * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+  for (let i = num; i > 0; i--) {
+    sum *= i;
+  };
+  return sum;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
 
+    if (count == 0){
+        return 0;
+    }
+    if (count < 0){
+        return "OOPS";
+    }
+
+    let prevNum = 1;
+    let prevPrevNum = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = prevNum + prevPrevNum;
+
+        prevPrevNum = prevNum;
+        prevNum = current;
+    }
+
+    return prevNum;
 };
 
 // Do not edit below this line
