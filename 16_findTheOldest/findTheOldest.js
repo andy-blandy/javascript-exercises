c7803e3ec3c6645970c6e6a58f7c88213bab6d22
andy-blandy <shsandrewdaniels@gmail.com>
2025-09-20 12:42:44 -0400

    Finished more exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        // CALL THE GET AGE FUNCTION FOR BOTH OLDEST AND CURRENT PERSON
+        // IF CURRENT PERSON IS OLDER, SET AS OLDEST
+        const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
+
+const getAge = function(death, birth) {
+    // INSERT CODE TO GET THE AGE
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
